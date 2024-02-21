c5757e9b1dbe412dca489fae7c9f65a0137024c0
M-Roesner <markus_roesner@web.de>
2024-02-21 20:16:14 +0100

    Adjusted text, due to finishing of the retraining.

--- aboutMe.php
+++ aboutMe.php
@@ -1,13 +1,14 @@
 <?php
-    $current_page = "aboutMe";
-    include("./private/pages/elements/head.php");
+$current_page = "aboutMe";
+include("./private/pages/elements/head.php");
 ?>
-    <title>Über mich!</title>
+<title>Über mich!</title>
 </head>
+
 <body>
     <div id="top" class="site">
         <?php
-            include("./private/pages/elements/header.php");
+        include("./private/pages/elements/header.php");
         ?>
 
         <main>
@@ -16,37 +17,54 @@
                 <section id="whoAmI">
                     <h3>Wer bin ich?</h3>
                     <div class="line-left">
+                        <p>
                             Hallo,
                         </p>
                         <p>
-                            mein Name ist Markus. 
+                            mein Name ist Markus.
                         </p>
                         <p>
-                            Aktuell mache ich eine Umschulung bei der SRH in Heidelberg, als Fachinformatiker mit der Fachrichtung Anwendungsentwicklung.
+                            Im Frühjahr 2024 habe ich meine Umschulung als Fachinformatiker mit Schwerpunkt
+                            Anwendungsentwicklung an der SRH in Heidelberg erfolgreich abgeschlossen. Während meiner
+                            Umschulung absolvierte ich ein Praktikum bei der Firma FutureNext GmbH in Mannheim, wo ich
+                            einen praxisnahen Einblick in die Arbeitsweise eines Programmierers erhielt. Dabei konnte
+                            ich Fähigkeiten in Technologien wie TypeScript und React erlernen.
+
                         </p>
                         <p>
-                            In den letzten 18 Jahren, war ich in einer Buchbinderei angestellt und habe dort verschiedene Produktionsanlagen geführt. Darunter waren ein Klebebinder, ein Sammelhefter, sowie diverse Kleinanlagen (z.B. Falzmaschinen, Schneidemaschinen und Pallettieranlagen).
+                            In den vergangenen 18 Jahren war ich in einer Buchbinderei angestellt und habe umfangreiche
+                            Erfahrungen in der Bedienung verschiedener Produktionsanlagen gesammelt. Dazu zählten unter
+                            anderem ein Klebebinder, ein Sammelhefter sowie diverse Kleinanlagen wie Falzmaschinen,
+                            Schneidemaschinen und Pallettieranlagen.
                         </p>
                         <p>
-                            Ich bin zum Schichtleiter aufgestiegen und habe verschiedene Projekte geleitet oder begleitet.
+                            Ich konnte mich zum stellvertretenden Schichtleiter hocharbeiten und hatte die Gelegenheit,
+                            verschiedene Projekte zu leiten oder aktiv mitzugestalten.
                         </p>
                         <p>
-                            Die Schließung der Firma habe ich genutzt, um mich neu zu orientieren. Meine Überlegungen haben mich in die IT-Welt geführt.
+                            Die Schließung der Firma habe ich als Chance genutzt, mich beruflich neu zu orientieren.
+                            Meine Überlegungen haben mich in die faszinierende Welt der IT geführt.
                         </p>
                     </div>
                 </section>
-                
-                <section id="whyIt" >
+
+                <section id="whyIt">
                     <h3>Warum ein Beruf in der Informatik?</h3>
                     <div class="line-left">
                         <p>
-                            Während meines letzten Jobs habe ich anfangs für meine Kollegen kleine automatisierte Tabellen in Excel erstellt, um die Arbeitsabläufe zu optimieren und Zeit zu sparen. Diese habe ich auch später zum Teil mit <span title="Visual Basic for Applications" class="infobox">VBA</span> verbessert.
+                            Während meines letzten Jobs habe ich anfangs für meine Kollegen kleine automatisierte
+                            Tabellen in Excel erstellt, um die Arbeitsabläufe zu optimieren und Zeit zu sparen. Diese
+                            habe ich auch später zum Teil mit <span title="Visual Basic for Applications"
+                                class="infobox">VBA</span> verbessert.
                         </p>
                         <p>
-                            Ebenso hatte ich für einen Sachbearbeiter ein komplexeres Pallettezettel-Programm mit Excel und VBA erstellt, welches eine Ein- und Ausgabe-Maske erhielt und die meisten Berechnungen von allein erledigte.
+                            Ebenso hatte ich für einen Sachbearbeiter ein komplexeres Pallettezettel-Programm mit Excel
+                            und VBA erstellt, welches eine Ein- und Ausgabe-Maske erhielt und die meisten Berechnungen
+                            von allein erledigte.
                         </p>
                         <p>
-                            Mich mit VBA auseinander zu setzen, war eine neue Erfahrung für mich. Das mir viel Spaß gemacht hat, sodass ich mich für den neuen Weg als Anwendungsentwickler entschied.
+                            Mich mit VBA auseinander zu setzen, war eine neue Erfahrung für mich. Das mir viel Spaß
+                            gemacht hat, sodass ich mich für den neuen Weg als Anwendungsentwickler entschied.
                         </p>
                     </div>
                 </section>
@@ -55,21 +73,24 @@
                     <h3>Fähigkeiten:</h3>
                     <div class="line-left">
                         <ul class="skills">
+                            <li>JS/TS <span>„<b>J</b>ava<b>S</b>cript“ / „<b>T</b>ype<b>S</b>cript“</span></li>
+                            <li>React</li>
                             <li>HTML <span>„<b>H</b>yper<b>t</b>ext <b>M</b>arkup <b>L</b>anguage“</span></li>
                             <li>CSS <span>„<b>C</b>ascading <b>S</b>tyle <b>S</b>heets“</span></li>
-                            <li>PHP <span>„<b>P</b>HP: <b>H</b>ypertext <b>P</b>reprocessor“, ursprünglich „<b>P</b>ersonal <b>H</b>ome <b>P</b>age Tools“</span></li>
+                            <li>PHP <span>„<b>P</b>HP: <b>H</b>ypertext <b>P</b>reprocessor“, ursprünglich
+                                    „<b>P</b>ersonal <b>H</b>ome <b>P</b>age Tools“</span></li>
                             <li>Java</li>
                             <li>DB <span>„<b>D</b>ata<b>b</b>ase“</span></li>
-                            <li>JS <span><b>J</b>ava<b>S</b>cript</span></li>
                             <li>VBA <span>„<b>V</b>isual <b>B</b>asic for <b>A</b>pplications“</span></li>
                         </ul>
                     </div>
                 </section>
 
                 <section id="whyJs">
-                    <h3>Warum <span title="JavaScript" class="infobox">JS</span> als zusätzliche Programmiersprache?</h3>
+                    <h3>Warum <span title="JavaScript" class="infobox">JS</span> als zusätzliche Programmiersprache?
+                    </h3>
                     <div class="line-left">
-                        <ul class="why-js">                           
+                        <ul class="why-js">
                             <li>Webauftritt, um mich zu präsentieren.</li>
                             <li>Einsatzbereich in Frontend und Backend, sowie Webapps.</li>
                             <li>Diese ist noch immer eine der meist genutzten Sprachen.</li>
@@ -80,18 +101,19 @@
                 <section id="contactMe">
                     <h3>Kontaktieren Sie mich über:</h3>
                     <div class="line-left">
-                        <ul class="contact">                           
-                            <li>contact@markus-roesner.de</li>
+                        <ul class="contact">
+                            <li>contact@markus-roesner.dev</li>
                             <li><a href="https://github.com/M-Roesner" target="_blank">[gitHub]</a></li>
                         </ul>
                     </div>
                 </section>
             </div>
         </main>
-        
+
         <?php include("./private/pages/elements/gotop.php"); ?>
         <?php include("./private/pages/elements/footer.php"); ?>
     </div>
-    
+
 </body>
+
 </html>
